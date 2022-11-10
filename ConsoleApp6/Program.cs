2cc2a045746161451620945a5c7f401b01acacef
Anton <antontokmakov757@gmail.com>
2022-11-10 19:53:09 +0700

    NewNewPrint

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApp6
             {
                 Console.WriteLine("Привет мир");
             }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Привет мир");
+            }
+            if (true) Console.WriteLine("bvkfdjv");
         }
     }
 }
